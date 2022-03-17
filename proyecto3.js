f9f87cf929f4c434584cb2cd64df8d157d1af328
larriety <infaurden@gmail.com>
2022-03-17 22:37:48 +0100

    correcciones Stefano

--- proyecto3.js
+++ proyecto3.js
@@ -1,60 +1,61 @@
 "use strict";
 
-//Poner el color que tengo que adivinar en el h2
+const State = {
+  colores: [],
+  colorGanador: null,
+};
+
+const liListElement = document.querySelectorAll(".cuadrado");
+const ulElement = document.querySelector("ul");
 
 const getRandom = (max) => {
   return Math.floor(Math.random() * (max + 1));
 };
 
 const getRandomColor = () => {
-  return `${getRandom(255)}, ${getRandom(255)}, ${getRandom(255)}`;
+  return `RGB(${getRandom(255)},${getRandom(255)},${getRandom(255)})`;
 };
-const randomColor = getRandomColor();
-
-// console.log(randomColor);
-
-let h2 = document.getElementById("colorobjetivo");
-h2.textContent = `RGB: (${randomColor})`;
-
-//Pintar los li de un color aleatorio y que uno de ellos sea el correcto
-
-const liListElement = document.querySelectorAll(".cuadrado");
 
 const cuadradosPintados = () => {
+  let contador = 0;
   for (const cuadrado of liListElement) {
-    cuadrado.style.backgroundColor = `
-    rgb(${getRandom(255)}, ${getRandom(255)}, ${getRandom(255)})`;
+    cuadrado.style.backgroundColor = State.colores[contador];
+    cuadrado.setAttribute("data-index", contador);
+    contador++;
   }
 };
-cuadradosPintados();
 
-//Juego con contador aciertos y fallos
-
-let mensajeEstado = document.querySelector("#message");
-
-function juego() {
-  let contadorAciertos = aciertos;
-  let contadorFallos = fallos;
-  let aciertosEstado = document.querySelector(".hits");
-  let fallosEstado = document.querySelector(".failures");
-  if ((cuadradoClickado == randomColor, contadorAciertos++)) {
-    message.textContent = "¡Acertaste!";
-    aciertosEstado.textContent = `${contadorAciertos}/3`;
-  } else if (contadorAciertos === 3) {
-    message.textContent = "¡Ganaste!";
-  } else if ((cuadradoClickado !== randomColor, contadorFallos++)) {
-    message.textContent = "Fallaste!";
-    fallosEstado.textContent = `${contadorFallos}/3`;
-  } else if (contadorFallos === 3) {
-    message.textContent = "¡Perdiste,vuelve a intentarlo!";
+function main() {
+  for (let index = 0; index < 6; index++) {
+    State.colores[index] = getRandomColor();
   }
+  const winner = getRandom(5);
+  State.colorGanador = winner;
+  let h2 = document.getElementById("colorobjetivo");
+  h2.textContent = State.colores[winner];
+
+  console.log(State.colores);
+  console.log(State.colorGanador);
+
+  cuadradosPintados();
 }
 
-//Definir cuadradoClickado
+main();
 
-//Al darle a juego nuevo resetear
+ulElement.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.matches("li")) {
+    const selecionUser = Number(target.getAttribute("data-index"));
+    console.log("TARGET", target);
+    console.log(selecionUser);
+    if (selecionUser === State.colorGanador) {
+      console.log("Ganaste chaval!");
+    } else {
+      console.log("Ententalo otra vez");
+    }
+  }
+});
 
-const clickReset = () => {
-  window.location.reload(); //preguntarle a Stefano si esto está bien así o hay algo mejor
-};
-resetButton = document.addEventListener("click", clickReset);
+let resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", main);
